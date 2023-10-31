ba98e40d6805068e4f7570d4f278a37b8e41112f
김상구 <spair0039@gmail.com>
2023-10-31 16:13:09 +0900

    CardList onPress Props, 클릭 시 효과 추가

--- src/components/common/CardList.tsx
+++ src/components/common/CardList.tsx
@@ -1,11 +1,15 @@
 import { ChevronRight } from "lucide-react-native";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, Pressable, View, GestureResponderEvent } from "react-native";
 
 interface PropsType{
     title: string;
+    onPress: (event: GestureResponderEvent) => void;
 }
 
-export default function CardList({title}:PropsType){
+export default function CardList({title, onPress}:PropsType){
+    const [IsPressing, setIsPrssing] = useState<boolean>(false)
+
     const style = StyleSheet.create({
         main:{
             display: 'flex',
@@ -13,6 +17,9 @@ export default function CardList({title}:PropsType){
             padding: 4,
             gap: 8,
             borderRadius: 4
+        },
+        mainPress:{
+            backgroundColor: '#F2F2F2',
         },
         text:{
             width: '100%',
@@ -22,9 +29,9 @@ export default function CardList({title}:PropsType){
     });
 
     return(
-        <View style={style.main}>
+        <Pressable onPress={onPress} onPressIn={()=>setIsPrssing(true)} onPressOut={()=>setIsPrssing(false)} style={[style.main ,IsPressing && style.mainPress]}>
             <Text style={style.text}>{title}</Text>
             <ChevronRight size={20} color="#B3B3B3"/>
-        </View>
+        </Pressable>
     );
 }
