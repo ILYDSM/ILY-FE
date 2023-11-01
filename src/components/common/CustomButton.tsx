adcbdc70d3461e04414f1de16f1139d6197c6f70
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-11-01 11:34:49 +0900

    fix: 버튼 수정

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,19 +1,52 @@
+import { platte } from '@/styles/platte';
 import React from 'react';
-import { GestureResponderEvent, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { GestureResponderEvent, StyleProp, StyleSheet, Text, TextProps, TouchableOpacity } from 'react-native';
+
+type ButtonColorType = 'Gray' | 'Black' | 'Transparent';
+type ButtonSizeType = 'L' | 'M' | 'S';
 
 interface ButtonPropsType {
-  titleColor?: string;
-  buttonColor?: string;
+  color?: ButtonColorType;
+  size?: ButtonSizeType;
   title?: string;
   onPress?: (event: GestureResponderEvent) => void;
 }
 
-export default ({ titleColor = '#FFF', buttonColor = '#000', title = 'button', onPress }: ButtonPropsType) => {
+export default ({ color = 'Black', title = 'button', onPress, size = 'L' }: ButtonPropsType) => {
   return (
-    <TouchableOpacity style={[styles.button, { backgroundColor: buttonColor }]} onPress={onPress}>
-      <Text style={[styles.title, { color: titleColor }]}>{title}</Text>
+    <TouchableOpacity
+      style={[styles.button, sizeGenerator(size), { backgroundColor: colorGenerator(color).backgroundColor }]}
+      onPress={onPress}
+    >
+      <Text style={[styles.title, { color: colorGenerator(color).fontColor, fontWeight: '600' }]}>{title}</Text>
     </TouchableOpacity>
   );
+};
+
+const colorGenerator = (colorType: ButtonColorType): { backgroundColor: string; fontColor: string } => {
+  switch (colorType) {
+    case 'Black':
+      return { backgroundColor: platte.gray100, fontColor: platte.gray00 };
+    case 'Gray':
+      return { backgroundColor: platte.gray10, fontColor: platte.gray100 };
+    case 'Transparent':
+      return { backgroundColor: 'transparent', fontColor: platte.gray100 };
+    default:
+      return { backgroundColor: platte.gray100, fontColor: platte.gray00 };
+  }
+};
+
+const sizeGenerator = (size: ButtonSizeType) => {
+  switch (size) {
+    case 'L':
+      return styles.Large;
+    case 'M':
+      return styles.Medium;
+    case 'S':
+      return styles.Small;
+    default:
+      return styles.Large;
+  }
 };
 
 const styles = StyleSheet.create({
@@ -21,14 +54,26 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 10,
-    borderRadius: 8,
-    borderWidth: 2,
-    paddingHorizontal: 10,
-    paddingVertical: 3,
-    marginHorizontal: 5,
   },
   title: {
-    fontSize: 15,
+    fontSize: 16,
+  },
+  Large: {
+    borderRadius: 8,
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    marginHorizontal: 4,
+  },
+  Medium: {
+    borderRadius: 80,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    marginHorizontal: 4,
+  },
+  Small: {
+    borderRadius: 26,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginHorizontal: 4,
   },
 });
