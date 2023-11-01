ebbf299c330fba08682e2c854d705308af42cbd3
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-11-01 11:16:15 +0900

    feat: 컬러 팔레트 추가

---
+++ src/styles/platte.ts
@@ -0,0 +1,13 @@
+export const platte = {
+  red: '#F24040',
+  gray00: '#FFFFFF',
+  gray05: '#F2F2F2',
+  gray10: '#E6E6E6',
+  gray20: '#CCCCCC',
+  gray30: '#B3B3B3',
+  gray50: '#808080',
+  gray70: '#4D4D4D',
+  gray80: '#333333',
+  gray90: '#1A1A1A',
+  gray100: '#000000',
+} as const;
