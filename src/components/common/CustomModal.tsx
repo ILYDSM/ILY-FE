c39587383bf17ccde2e8dc8ab23207a3e08636a6
김상구 <spair0039@gmail.com>
2023-11-03 08:22:12 +0900

    refactor: style 함수 이동

--- src/components/common/CustomModal.tsx
+++ src/components/common/CustomModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Modal from 'react-native-modal'
 import CustomButton from "./CustomButton";
@@ -11,55 +11,21 @@ interface PrposType{
 }
 
 export default function CustomModal({ IsOpen, setIsOpen, children, title }:PrposType){
-    const style = StyleSheet.create({
-        modal:{
-            backgroundColor:'white',
-            padding: 16,
-            borderRadius: 20,
-            width: '100%',
-            display: 'flex',
-            gap: 12,
-        },
-        modalHandle:{
-            width:40,
-            height:8,
-            backgroundColor:'#E6E6E6',
-            borderRadius:99,
-
-        },
-        modalHandleContainer:{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center'
-        },
-        title:{
-            width: '100%',
-            fontSize: 28,
-            fontWeight: '700',
-        },
-        backdrop:{
-            margin: 8,
-            display: "flex",
-            alignItems: 'flex-end',
-            flexDirection:'column',
-        }
-    })
-
     function closeModal(){
         setIsOpen(false);
     }
-
+    
     return(
         <Modal
-            hasBackdrop
-            backdropOpacity={0.25}
-            isVisible={IsOpen}
-            onBackdropPress={closeModal}
-            onBackButtonPress={closeModal}
-            onSwipeComplete={closeModal}
-            backdropTransitionInTiming={150}
-            backdropTransitionOutTiming={150}
-            style={style.backdrop}
+        hasBackdrop
+        backdropOpacity={0.25}
+        isVisible={IsOpen}
+        onBackdropPress={closeModal}
+        onBackButtonPress={closeModal}
+        onSwipeComplete={closeModal}
+        backdropTransitionInTiming={150}
+        backdropTransitionOutTiming={150}
+        style={style.backdrop}
         >
             <View style={{flex:1}}/>
             <View style={style.modal}>
@@ -73,3 +39,37 @@ export default function CustomModal({ IsOpen, setIsOpen, children, title }:Prpos
         </Modal>
     );
 }
+
+const style = StyleSheet.create({
+    modal:{
+        backgroundColor:'white',
+        padding: 16,
+        borderRadius: 20,
+        width: '100%',
+        display: 'flex',
+        gap: 12,
+    },
+    modalHandle:{
+        width:40,
+        height:8,
+        backgroundColor:'#E6E6E6',
+        borderRadius:99,
+
+    },
+    modalHandleContainer:{
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    title:{
+        width: '100%',
+        fontSize: 28,
+        fontWeight: '700',
+    },
+    backdrop:{
+        margin: 8,
+        display: "flex",
+        alignItems: 'flex-end',
+        flexDirection:'column',
+    }
+})
