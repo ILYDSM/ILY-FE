991237c74d0c62a5a101a8cdd81a2eb799bf7562
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-11-07 19:45:36 +0900

    fix: Input 이름 변경 및 isError 추가

---
+++ src/components/common/CustomInput.tsx
@@ -0,0 +1,61 @@
+import { platte } from '@/styles/platte';
+import { StyleSheet, TextInput, View, Text, TextInputProps } from 'react-native';
+import { ReactNode } from 'react';
+
+interface CustomInputPropsType extends TextInputProps {
+  text: string;
+  description?: string;
+  icon?: ReactNode;
+  isError?: boolean;
+}
+
+export default ({ text, description, icon, isError = false, ...props }: CustomInputPropsType) => {
+  return (
+    <View>
+      <View style={[styles.container, isError && { borderColor: platte.red, borderWidth: 2 }]}>
+        <Text>{text}</Text>
+        <TextInput
+          {...props}
+          selectionColor={platte.gray100}
+          style={[styles.input, !!icon && { paddingRight: 36 }]}
+          placeholderTextColor={platte.gray100}
+        />
+        {icon && <View style={styles.icon}>{icon}</View>}
+      </View>
+      {description && <Text style={[styles.description, isError && { color: platte.red }]}>{description}</Text>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    backgroundColor: 'tranparent',
+    paddingVertical: 12,
+    paddingHorizontal: 14,
+    borderColor: platte.gray30,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  input: {
+    height: 25,
+    padding: 2,
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: '500',
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: platte.gray50,
+    marginTop: 4,
+  },
+  icon: {
+    position: 'absolute',
+    right: 14,
+    bottom: 14,
+  },
+});
