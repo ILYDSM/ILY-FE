191d71f62a18da4b4a34028f4e27170ff008130a
jikwan <jikwan0327@gmail.com>
2023-11-08 09:01:19 +0900

    ffix: 네비게이션 추가 및 수정

---
+++ src/constants/mock.ts
@@ -0,0 +1,20 @@
+export const DATA = [
+  {
+    id: '1',
+    title: '모임1',
+    description: '영화 모임',
+    headCount: 10,
+  },
+  {
+    id: '2',
+    title: '모임2',
+    description: '독서 모임',
+    headCount: 1,
+  },
+  {
+    id: '3',
+    title: '모임3',
+    description: '프로젝트 모임',
+    headCount: 3,
+  },
+];
