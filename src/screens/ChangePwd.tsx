2e8bbe0a03d89776ffff1c0803eba349d265b614
jikwan <jikwan0327@gmail.com>
2023-11-08 14:59:11 +0900

    feat: 비밀번호 변경 이메일 입력 페이지

--- src/screens/ChangePwd.tsx
+++ src/screens/ChangePwd.tsx
@@ -1,14 +1,12 @@
 import CustomButton from '@/components/common/CustomButton';
 import TitleBar from '@/components/common/TitleBar';
-import { View, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParam } from '@/utils/RootStackParam';
 import { useForm, Controller } from 'react-hook-form';
 import CustomInput from '@/components/common/CustomInput';
-import { useState } from 'react';
-import { EyeOff, Eye } from 'lucide-react-native';
 import { platte } from '@/styles/platte';
 
 export default () => {
@@ -16,78 +14,44 @@ export default () => {
   const { control, handleSubmit } = useForm({
     defaultValues: {
       email: '',
-      secret_key: '',
     },
   });
 
-  const [isPasswordOpen, setIsPasswordOpen] = useState(false);
-
-  const handlePasswordEye = () => {
-    setIsPasswordOpen((prev) => !prev);
-  };
-
   return (
-    <SafeAreaView>
-      <TitleBar title="비밀번호 변경" />
+    <SafeAreaView style={{ flex: 1 }}>
+      <TitleBar title="비밀번호 변경" onPress={() => navigation.goBack()} />
       <View
         style={{
           flex: 1,
           width: '100%',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
+          paddingHorizontal: 16,
+          gap: 20,
         }}
       >
-        <View style={{ width: '90%', flex: 9 }}>
-          <View style={{ flexDirection: 'column', gap: 20 }}>
-            <Controller
-              control={control}
-              name="email"
-              rules={{
-                required: true,
-              }}
-              render={({ field: { onChange, value } }) => (
-                <CustomInput text="이메일" onChangeText={onChange} value={value} autoComplete="email" />
-              )}
-            />
-            <Controller
-              control={control}
-              name="secret_key"
-              rules={{
-                required: true,
-              }}
-              render={({ field: { onChange, value } }) => (
-                <CustomInput
-                  text="비밀번호"
-                  description="8~20자 이내, 알파벳 대소문자, 숫자를 포함해야 해요"
-                  icon={
-                    <TouchableOpacity onPress={handlePasswordEye}>
-                      {isPasswordOpen ? (
-                        <Eye color={platte.gray100} size={20} />
-                      ) : (
-                        <EyeOff color={platte.gray100} size={20} />
-                      )}
-                    </TouchableOpacity>
-                  }
-                  onChangeText={onChange}
-                  value={value}
-                  secureTextEntry={!isPasswordOpen}
-                  autoComplete="password"
-                />
-              )}
-            />
-          </View>
-        </View>
-        <View style={{ width: '90%' }}>
-          <CustomButton
-            disabled
-            title="로그인"
-            onPress={() => {
-              // handleSubmit(onSubmit);
-              navigation.navigate('Main');
+        <Text style={{ color: platte.gray100, fontSize: 16, fontWeight: '500' }}>
+          본인 확인을 위해 가입할 때 사용한 이메일 인증이 필요해요
+        </Text>
+        <View style={{ flexDirection: 'column', gap: 20 }}>
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required: true,
             }}
+            render={({ field: { onChange, value } }) => (
+              <CustomInput text="이메일" onChangeText={onChange} value={value} autoComplete="email" />
+            )}
           />
         </View>
+      </View>
+      <View style={{ width: '100%', paddingHorizontal: 16 }}>
+        <CustomButton
+          title="→ 다음"
+          onPress={() => {
+            // handleSubmit(onSubmit);
+            navigation.navigate('VerifyEmail');
+          }}
+        />
       </View>
     </SafeAreaView>
   );
