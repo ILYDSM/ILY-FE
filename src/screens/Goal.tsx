b86bf1c62850bfbb4c2932ca9ea0917676c4c7b3
jikwan <jikwan0327@gmail.com>
2023-11-14 23:41:28 +0900

    fix: 목표 화면 컴포넌트 배치 수정

--- src/screens/Goal.tsx
+++ src/screens/Goal.tsx
@@ -9,8 +9,13 @@ const Goal = () => {
       <PageTitle title="모든 목표" />
       <View style={{ paddingHorizontal: 16, gap: 16 }}>
         <CustomButton title="+  새 목표 만들기" size="M" />
-        <View style={{ gap: 12, flexWrap: 'wrap' }}>
-          <GoalCard text="관광통역안내사 취득" color="#339988" />
+        <View style={{ gap: 12, flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{ width: '48%' }}>
+            <GoalCard text="관광통역안내사 취득" color="#339988" />
+          </View>
+          <View style={{ width: '48%' }}>
+            <GoalCard text="관광통역안내사 취득" color="#339988" />
+          </View>
         </View>
       </View>
     </>
