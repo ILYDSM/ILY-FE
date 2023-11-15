89fe92ba60a18fcbc53e62d50adf7aad2dfa12bd
eternrust <eternrust@dsm.hs.kr>
2023-11-15 22:01:02 +0900

    feat: calendar publishing

--- src/screens/GoalCalendar.tsx
+++ src/screens/GoalCalendar.tsx
@@ -1,7 +1,189 @@
-import { Text } from 'react-native';
+import TitleBar from '@/components/common/TitleBar';
+import { platte } from '@/styles/platte';
+import { RootStackParam } from '@/utils/RootStackParam';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useEffect, useState } from 'react';
+import { Dimensions, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+interface ItemPropsType {
+  size: number;
+  day: number;
+  isCheck: boolean;
+}
+
+const Item = ({ size, day, isCheck }: ItemPropsType) => {
+  return (
+    <View style={[styles.recordBox, styles[isCheck ? 'enabled' : 'disabled'], {
+      width: size,
+      height: size
+    }]}>
+      <Text style={styles.recordText}>{day}</Text>
+    </View>
+  )
+}
+
+interface CalendarType {
+  title: string;
+  dates: CheckType[]
+}
+
+interface CheckType {
+  day: number;
+  isCheck: boolean;
+}
 
 const GoalCalendar = () => {
-  return <Text>목표 달성 달력</Text>;
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParam>>();
+  const windowSize = Dimensions.get('window');
+  const [containerWidth, setContainerWidth] = useState(windowSize.width);
+  const numColumns = 7;
+  const [CalendarData, setCalendarData] = useState<CalendarType[]>([
+    {
+      title: '6월',
+      dates: [{
+        day: 23,
+        isCheck: true
+      }]
+    },
+    {
+      title: '5월',
+      dates: [{
+        day: 10,
+        isCheck: true
+      }]
+    }
+  ])
+
+  useEffect(() => {
+    DataChecking(Data)
+  }, [])
+
+  const DataChecking = (data?: string[]) => {
+    const sortedData = data?.sort((a, b) => {
+      return new Date(b).getTime() - new Date(a).getTime()
+    }) || []
+    const lastDate = sortedData[0] ? new Date(sortedData[0]) : new Date();
+    const firstDate = sortedData.at(-1) ? new Date(sortedData.at(-1) as string) : new Date();
+    let dataSliceTmp = 0;
+    const length = (lastDate.getFullYear() - firstDate.getFullYear()) * 12 + (lastDate.getMonth() - firstDate.getMonth()) + 1
+
+    setCalendarData(Array.from({ length }, (_, index) => {
+      const date = new Date(lastDate.getFullYear(), lastDate.getMonth() - index + 1, 0)
+      const maxDay = date.getDate();
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1;
+      const twoMonth = `0${month}`.slice(-2)
+      const data = sortedData.slice(dataSliceTmp).filter(value => value.match(`${year}-${twoMonth}`));
+      dataSliceTmp += data.length;
+      
+      return {
+        title: `${month}월`,
+        dates: Array.from({ length: maxDay }, (_, index) => {
+          const twoDay = `0${maxDay - index}`.slice(-2)
+          return {
+            day: maxDay - index,
+            isCheck: data.indexOf(`${year}-${twoMonth}-${twoDay}`) !== -1
+          }
+        })
+      }
+    }))
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <TitleBar title='목표 달성 달력' onPress={() => navigation.goBack()} />
+        <View style={styles.contentBox}>
+          <View style={styles.borderBox}>
+            <Text style={styles.checkText}>14일 연속으로 달성 중</Text>
+          </View>
+          {
+            CalendarData.map((data, index) => (
+              <View style={styles.calendarBox} key={index}>
+                <Text style={styles.recordTitle}>{data.title}</Text>
+                <FlatList
+                  data={data.dates}
+                  renderItem={({ item }) => (
+                    <Item size={(containerWidth - 60) / numColumns} day={item.day} isCheck={item.isCheck} />
+                  )}
+                  numColumns={7}
+                  keyExtractor={(_, index) => `${index}`}
+                  onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}
+                  ItemSeparatorComponent={() => <View style={styles.rowGap} />}
+                  columnWrapperStyle={styles.cloumnGap}
+                  scrollEnabled={false}
+                />
+              </View>
+            ))
+          }
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
 };
 
 export default GoalCalendar;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+    width: '100%'
+  },
+  contentBox: {
+    gap: 10,
+    width: '100%'
+  },
+  borderBox: {
+    gap: 8,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: platte.gray10,
+    padding: 12,
+  },
+  checkText: {
+    fontSize: 20,
+    fontWeight: '700'
+  },
+  calendarBox: {
+    gap: 4,
+    width: '100%'
+  },
+  recordTitle: {
+    fontSize: 16,
+    fontWeight: '700'
+  },
+  recordBox: {
+    width: 51,
+    height: 51,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  recordText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#FFFFFF'
+  },
+  enabled: {
+    backgroundColor: '#000000',
+  },
+  disabled: {
+    backgroundColor: '#B3B3B3',
+  },
+  rowGap: {
+    height: 4
+  },
+  cloumnGap: {
+    gap: 4
+  }
+})
+
+const Data = [
+  "2023-05-01",
+  "2023-05-10",
+  "2023-06-23",
+  "2023-11-14",
+  "2023-10-17",
+]
