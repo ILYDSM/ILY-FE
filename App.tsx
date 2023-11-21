0b1dff28d7fd5d4c4c50c64492bf4393ce9e177e
김상구 <spair0039@gmail.com>
2023-11-21 11:07:40 +0900

    feat: 불러온 폰트 Static 으로 교체

--- App.tsx
+++ App.tsx
@@ -1,31 +1,43 @@
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as SplashScreen from 'expo-splash-screen';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import RootNavigation from './src/navigations/RootNavigation';
-import { StatusBar } from 'expo-status-bar';
-import * as NavigationBar from 'expo-navigation-bar'
-import { useEffect } from 'react';
-import { Platform } from 'react-native';
-import { useFonts } from 'expo-font';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import RootNavigation from "./src/navigations/RootNavigation";
+import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar"
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import { setCustomText } from "react-native-global-props";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [isFontLoaded] = useFonts({
-    "Pretendard Variable": require('@/../assets/fonts/PretendardVariable.ttf'),
+    "Ptd EL": require("@/../assets/fonts/Pretendard-ExtraLight.otf"),
+    "Ptd L": require("@/../assets/fonts/Pretendard-Light.otf"),
+    "Ptd M": require("@/../assets/fonts/Pretendard-Medium.otf"),
+    "Ptd": require("@/../assets/fonts/Pretendard-Regular.otf"),
+    "Ptd SB": require("@/../assets/fonts/Pretendard-SemiBold.otf"),
+    "Ptd B": require("@/../assets/fonts/Pretendard-Bold.otf"),
+    "Ptd EB": require("@/../assets/fonts/Pretendard-ExtraBold.otf"),
+    "Ptd BL": require("@/../assets/fonts/Pretendard-Black.otf"),
   })
 
   useEffect(() => {
+    NavigationBar.setBackgroundColorAsync("#FFFFFF");
+    NavigationBar.setButtonStyleAsync("dark");
     SplashScreen.hideAsync();
-    if(Platform.OS === 'android'){
-      NavigationBar.setBackgroundColorAsync('#FFFFFF');
-    }
-  }, [isFontLoaded]);
-
+  }, []);
+  
   if (!isFontLoaded) {
     return null;
   }
-
+  
+  setCustomText({
+    style:{
+      fontFamily: "Ptd"
+    }
+  })
+  
   return (
     <SafeAreaProvider>
       <NavigationContainer
@@ -33,7 +45,7 @@ export default function App() {
           ...DefaultTheme,
           colors: {
             ...DefaultTheme.colors,
-            background: '#ffffff',
+            background: "#ffffff",
           },
         }}
       >
