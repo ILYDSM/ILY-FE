3e83b0e8f06dbafe1496d2a80fca9c58766be4ca
eternrust <eternrust@dsm.hs.kr>
2023-11-27 08:44:00 +0900

    feat: 399 에러를 해결하기 위한 노력

--- src/apis/target/index.ts
+++ src/apis/target/index.ts
@@ -1,7 +1,13 @@
+import { getItem } from '@/utils/AsyncStorage';
 import { instance } from '../axios';
 
 const router = '/target';
 
 export const createMandalArt = async (body: CreateMandalArtRequest) => {
-  return await instance.post(`${router}`, body);
+  const accessToken = await getItem('access_token');
+  return await instance.post(`${router}`, body, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+  });
 };
