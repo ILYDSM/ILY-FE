099d947d4e8812f4ed663160d1338527abf352d8
jikwan <jikwan0327@gmail.com>
2023-12-05 19:26:09 +0900

    fix: style 수정

--- src/components/common/MandalArt/MandalArtEdit.tsx
+++ src/components/common/MandalArt/MandalArtEdit.tsx
@@ -13,23 +13,64 @@ const MandalArtEdit = ({ data, title, onChangeData, theme = GrayTheme }: MandalA
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.innerContainer}>
-        <TextInput multiline value={data[0]} style={[styles.item, theme?.sub[0] ?? theme?.sub[0]]} onChangeText={(text: string) => onChangeData([text, ...data.slice(1)])}/>
-        <TextInput multiline value={data[1]} style={[styles.item, theme?.sub[1] ?? theme?.sub[0]]} onChangeText={(text: string) => onChangeData([data[0], text, ...data.slice(2)])}/>
-        <TextInput multiline value={data[2]} style={[styles.item, theme?.sub[2] ?? theme?.sub[0]]} onChangeText={(text: string) => onChangeData([...data.slice(0, 2), text, ...data.slice(3)])}/>
+        <TextInput
+          multiline
+          numberOfLines={5}
+          value={data[0]}
+          style={[styles.item, theme?.sub[0] ?? theme?.sub[0]]}
+          onChangeText={(text: string) => onChangeData([text, ...data.slice(1)])}
+        />
+        <TextInput
+          multiline
+          value={data[1]}
+          style={[styles.item, theme?.sub[1] ?? theme?.sub[0]]}
+          onChangeText={(text: string) => onChangeData([data[0], text, ...data.slice(2)])}
+        />
+        <TextInput
+          multiline
+          value={data[2]}
+          style={[styles.item, theme?.sub[2] ?? theme?.sub[0]]}
+          onChangeText={(text: string) => onChangeData([...data.slice(0, 2), text, ...data.slice(3)])}
+        />
       </View>
       <View style={styles.innerContainer}>
-        <TextInput multiline value={data[3]} style={[styles.item, theme?.sub[3] ?? theme?.sub[0]]} onChangeText={(text: string) => onChangeData([...data.slice(0, 3), text, ...data.slice(4)])}/>
+        <TextInput
+          multiline
+          value={data[3]}
+          style={[styles.item, theme?.sub[3] ?? theme?.sub[0]]}
+          onChangeText={(text: string) => onChangeData([...data.slice(0, 3), text, ...data.slice(4)])}
+        />
         <MandalaArtItem data={title} style={theme?.title} />
-        <TextInput multiline value={data[4]} style={[styles.item, theme?.sub[4] ?? theme?.sub[0]]} onChangeText={(text: string) => onChangeData([...data.slice(0, 4), text, ...data.slice(5)])}/>
+        <TextInput
+          multiline
+          value={data[4]}
+          style={[styles.item, theme?.sub[4] ?? theme?.sub[0]]}
+          onChangeText={(text: string) => onChangeData([...data.slice(0, 4), text, ...data.slice(5)])}
+        />
       </View>
       <View style={styles.innerContainer}>
-        <TextInput multiline value={data[5]} style={[styles.item, theme?.sub[5] ?? theme?.sub[0]]} onChangeText={(text: string) => onChangeData([...data.slice(0, 5), text, ...data.slice(6)])}/>
-        <TextInput multiline value={data[6]} style={[styles.item, theme?.sub[6] ?? theme?.sub[0]]} onChangeText={(text: string) => onChangeData([...data.slice(0, 6), text, data[7]])}/>
-        <TextInput multiline value={data[7]} style={[styles.item, theme?.sub[7] ?? theme?.sub[0]]} onChangeText={(text: string) => onChangeData([...data.slice(0, 7), text])}/>
+        <TextInput
+          multiline
+          value={data[5]}
+          style={[styles.item, theme?.sub[5] ?? theme?.sub[0]]}
+          onChangeText={(text: string) => onChangeData([...data.slice(0, 5), text, ...data.slice(6)])}
+        />
+        <TextInput
+          multiline
+          value={data[6]}
+          style={[styles.item, theme?.sub[6] ?? theme?.sub[0]]}
+          onChangeText={(text: string) => onChangeData([...data.slice(0, 6), text, data[7]])}
+        />
+        <TextInput
+          multiline
+          value={data[7]}
+          style={[styles.item, theme?.sub[7] ?? theme?.sub[0]]}
+          onChangeText={(text: string) => onChangeData([...data.slice(0, 7), text])}
+        />
       </View>
     </SafeAreaView>
   );
-}
+};
 
 export default MandalArtEdit;
 
